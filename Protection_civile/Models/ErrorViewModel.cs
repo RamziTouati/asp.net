d1c2334336f00df85e18073587bc41c7b9d4f9cd
TRamzi <ramzitouati1@gmail.com>
2022-01-08 16:21:32 +0100

    first commit

---
+++ Protection_civile/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Protection_civile.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
