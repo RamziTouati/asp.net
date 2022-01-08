8df591c937f256812e10b330489de30575a80fcc
TRamzi <ramzitouati1@gmail.com>
2022-01-08 20:12:27 +0100

    add class citoyen

---
+++ Protection_civile/Models/Citoyen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Protection_civile.Models
+{
+    public class Citoyen
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nom Obligatoire")]
+        [StringLength(30, MinimumLength = 3)]
+        public string Nom { get; set; }
+        [Required(ErrorMessage = "Prénom Obligatoire")]
+        [StringLength(30, MinimumLength = 3)]
+        public string Prenom { get; set; }
+        [Display(Name = "Date De Naissance")]
+        public DateTime DateNaissance {get; set; }
+        
+    }
+}
